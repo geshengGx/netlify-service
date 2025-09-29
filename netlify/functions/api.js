b0e0c36671e223dd146f7286482b538981e696f2
geshengGx <943267022@qq.com>
2025-09-29 10:25:33 +0800

    添加静态资源访问

--- netlify/functions/api.js
+++ netlify/functions/api.js
@@ -1,16 +1,14 @@
 import express from "express";
 import serverless from "serverless-http";
 import cors from "../../config/cors.js";
-import { resolve, dirname } from 'path';
-import { fileURLToPath } from 'url';
+import { resolve } from 'path';
 
 // 路由导入
 import user from "../../routers/user.js"
 
 const api = express();
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
-const staticPath = resolve(__dirname, '../../public');
+const projectRoot = process.cwd(); // 项目根目录
+const staticPath = resolve(projectRoot, 'public'); //静态资源目录
 
 // 将 public 目录设置为静态资源目录
 api.use('/static', express.static(staticPath));
@@ -21,7 +19,8 @@ api.use(cors); // 跨域
 // 主页
 api.get('/', (req, res) => {
   // 使用 path.join 构建安全的绝对路径
-  res.sendFile(resolve(staticPath, 'index.html'));
+  // res.sendFile(resolve(staticPath, 'index.html'));
+  res.send('Hello World!---->'+ staticPath);
 });
 
 
